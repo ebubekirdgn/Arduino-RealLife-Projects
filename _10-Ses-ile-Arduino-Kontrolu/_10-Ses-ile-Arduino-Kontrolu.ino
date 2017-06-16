9d8c416e531b0dc2380bbafe9e4c1564ad18e411
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2017-06-16 14:18:56 +0300

    Ses-ile-Arduino-Kontrolu

---
+++ _10-Ses-ile-Arduino-Kontrolu/_10-Ses-ile-Arduino-Kontrolu.ino
@@ -0,0 +1,23 @@
+int led = 13; //Led'in bagli oldugu pin
+int esik_degeri = 500; // Ses icin bizim sectigimiz esik degeri
+int deger; // Okunan verinin aktarılacagi degisken
+
+void setup() 
+{
+Serial.begin(9600); //Seri iletim basliyor.
+pinMode(led, OUTPUT);
+}
+
+void loop()
+{
+    deger = analogRead(A0); // Analog-0'dan gelen veriyi oku ve deger'e aktar.
+    if(deger >= esik_degeri)
+    {
+      //deger disaridan gelen ses sinyali kart degerinden buyukse 
+      digitalWrite(led, HIGH);//Led'i yak
+    }
+    else
+    {
+      digitalWrite(led,LOW); //degilse ledi sondur.
+    }
+}
