33da5082a49d24c985da8531eb4cc4c7d2212ce9
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2017-06-16 14:18:47 +0300

    Ses-ile-Besik-Kontrol_

---
+++ _9-Ses-ile-Besik-Kontrol_/_9-Ses-ile-Besik-Kontrol_.ino
@@ -0,0 +1,42 @@
+int motor_Pin = 9; // Motorun bagli oldugu PWM pini
+int esik_degeri = 500; // Bebek sesi icin sectigimiz esik degeri
+int deger; // Okunan analog verinin aktarılacagı degisken
+
+void setup() 
+{
+  Serial.begin(9600); // Seri iletim basliyor...
+  pinMode(motor_Pin ,OUTPUT);  // Motor Pini cikis olarak kullanilacak...
+}
+
+void loop()
+{
+   deger = analogRead(A0); //Bebegin sesi alinip degiskene aktariliyor.
+   if(deger >= esik_degeri)  // Eger ses esige esit veya buyukse
+   {
+    for(int i=0;i<256;i++) // Motor hizi artsın.
+    {
+      analogWrite(motor_Pin, i);  //calismaya baslasin.
+      delay(10); // Cakisma olmamasi icin gecikme veriyoruz.
+    }
+    delay(3000); // Motor 3 sn. calisiyor.
+     for(int i=255;i>=0;i--)
+    {
+      analogWrite(motor_Pin, i);  //duruyor
+      delay(10); // Cakisma olmamasi icin gecikme veriyoruz.
+    }
+     delay(3000); // Motor 3 sn. calisiyor.
+   }
+   else
+   {
+     analogWrite(motor_Pin, LOW); //bebegin sesi az ise motor duruyor.  
+   }
+
+}
+
+
+
+
+
+
+
+
